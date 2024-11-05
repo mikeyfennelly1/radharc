9e8827cac0c962fb82c250b21f34f71f961e4f07
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-11-05 17:19:47 +0000

    variable block removed from conv_operations. Functions made global

--- core/parse/kvp/conv_operations.go
+++ core/parse/kvp/conv_operations.go
@@ -6,49 +6,31 @@ import (
 	"strings"
 )
 
-var (
-	STR_TO_STR = ConvOperation[string]{
-		function: strToStr,
-	}
-	STR_TO_INT = ConvOperation[int64]{
-		function: strToInt,
-	}
-	STR_TO_FLOAT64 = ConvOperation[float64]{
-		function: strToFloat64,
-	}
-	STR_TO_STR_SLICE = ConvOperation[[]string]{
-		function: strToStrSlice,
-	}
-	YES_NO_TO_BOOL = ConvOperation[bool]{
-		function: yesNoToBool,
-	}
-)
-
 // return passed string
-func strToStr(s string) (string, error) {
+func StrToStr(s string) (string, error) {
 	return s, nil
 }
 
 // Parse string to int64
-func strToInt(s string) (int64, error) {
+func StrToInt(s string) (int64, error) {
 	result, err := strconv.ParseInt(s, 10, 64)
 	return result, err
 }
 
 // string to float64
-func strToFloat64(s string) (float64, error) {
+func StrToFloat64(s string) (float64, error) {
 	result, err := strconv.ParseFloat(s, 64)
 	return result, err
 }
 
 // Convert string to string slice
-func strToStrSlice(s string) ([]string, error) {
+func StrToStrSlice(s string) ([]string, error) {
 	slice := strings.Split(s, " ")
 	return slice, nil
 }
 
 // Convert yes/no to boolean value
-func yesNoToBool(s string) (bool, error) {
+func YesNoToBool(s string) (bool, error) {
 	if s == "yes" {
 		return true, nil
 	} else if s == "no" {
