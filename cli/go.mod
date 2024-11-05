f7909baf658746b22ca79f2eca0bec462808ce81
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-11-05 10:12:16 +0000

    KVPParser created, not tested

--- cli/go.mod
+++ cli/go.mod
@@ -2,6 +2,4 @@ module github.com/mikeyfennelly1/radharc/cli
 
 go 1.18
 
-require (
-	github.com/mikeyfennelly1/radharc latest
-)
+require github.com/mikeyfennelly1/radharc v0.0.0-20241103130056-80010b5f5840
--- core/parse/kvp_parser.go
+++ core/parse/kvp_parser.go
@@ -4,19 +4,39 @@
 package parse
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 	"strings"
 )
 
-type genericKVP[T any] struct {
+type Converter interface {
+	Convert(string) (any, error)
+}
+
+type convOperation[T any] struct {
+	function func(string) (T, error)
+}
+
+func (c convOperation[T]) Convert(s string) (any, error) {
+	return c.function(s)
+}
+
+var conv_int64 = func(s string) (int64, error) {
+	parsedInt, err := strconv.ParseInt(s, 10, 64)
+	return parsedInt, err
+}
+
+type KVP[T any] struct {
 	key   string
 	value T
 }
 
 // Splits a key-value pair string at a splitter substring, and converts it to another
 // type via the 'convert' function passed as an arg.
-func splitKVP[T any](input string, splitter string, convert func(string) (T, error)) (*genericKVP[T], error) {
+func lineParser[T any](input string, splitter string, convert func(string) (T, error)) (*KVP[T], error) {
 	splitString := strings.Split(input, splitter)
 	if len(splitString) < 2 {
 		return nil, fmt.Errorf("input string: '%s' does not contain enough parts", input)
@@ -27,7 +47,7 @@ func splitKVP[T any](input string, splitter string, convert func(string) (T, err
 		return nil, err
 	}
 
-	kvp := &genericKVP[T]{
+	kvp := &KVP[T]{
 		key:   strings.TrimSpace(splitString[0]),
 		value: value,
 	}
@@ -35,16 +55,47 @@ func splitKVP[T any](input string, splitter string, convert func(string) (T, err
 	return kvp, nil
 }
 
-// convert a string to an integer
-func stringToInt(S string) (int, error) {
-	return strconv.Atoi(S)
+type KVPParser struct {
+	opMap     map[string]convOperation[interface{}]
+	parserRan bool
+	resultMap map[string]interface{}
 }
 
-func main() {
-	kvp, err := splitKVP("age : 30", ":", stringToInt)
+func (cm *KVPParser) AddConvOperation(key string, convOp convOperation[interface{}]) {
+	cm.opMap[key] = convOp
+}
+
+func (parser *KVPParser) Parse(absoluteFilePath string, separator string) {
+	file, err := os.Open(absoluteFilePath)
 	if err != nil {
-		fmt.Errorf("Error: %s", err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Printf("Key: %s, Value: %d\n", kvp.key, kvp.value)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+
+		separatorIndex := strings.Index(line, separator)
+		if separatorIndex == -1 {
+			continue
+		}
+		key := strings.TrimSpace(line[:separatorIndex])
+		if convOperationValue, found := parser.opMap[key]; found {
+			stringVal := strings.TrimSpace(line[separatorIndex:])
+			value, err := convOperationValue.function(stringVal)
+			if err != nil {
+				parser.resultMap[key] = value
+			}
+			continue
+		} else {
+			continue
+		}
+	}
+	parser.parserRan = true
 }
