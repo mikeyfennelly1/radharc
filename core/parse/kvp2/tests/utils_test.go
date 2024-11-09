50cff7596783dbc7eb4888b34f4a9c7f6acf3092
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-11-09 16:45:44 +0000

    test for GetStringKeyVal added

---
+++ core/parse/kvp2/tests/utils_test.go
@@ -0,0 +1,26 @@
+package parse
+
+import (
+	parse "github.com/mikeyfennelly1/radharc/core/parse/kvp2"
+	"testing"
+)
+
+func TestGetStringKeyVal(t *testing.T) {
+	tests := []struct {
+		name            string
+		line            string
+		keyValSeparator string
+		want            *parse.StringKeyVal
+	}{
+		{"Perfect case (key, value and separator exist)", "key : value", ":", &parse.StringKeyVal{Key: "key", Val: "value"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := parse.GetStringKeyVal(tt.line, tt.keyValSeparator)
+			if got.Val != tt.want.Val || got.Key != tt.want.Key {
+				t.Errorf("%s: parse.GetStringKeyVal(\"%s\", \"%s\") => %s; want: %s\n", tt.name, tt.line, tt.keyValSeparator, got, tt.want)
+			}
+		})
+	}
+}
