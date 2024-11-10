e88d54ed90c1d9cc04466b0d490d823ae0a020f9
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-11-10 18:29:44 +0000

    renaming and extra error check in parse_line

--- core/parse/kvp2/parse_line.go
+++ core/parse/kvp2/parse_line.go
@@ -1,36 +1,42 @@
 package parse
 
+import (
+	"fmt"
+)
+
 type KVP struct {
-	key   string
-	value interface{}
+	Key   string
+	Value interface{}
 }
 
 type Parser struct {
-	convOpMap map[string]ConversionOperation
+	ConvOpMap map[string]ConversionOperation
 }
 
-func (parser *Parser) parseLine(line string, keyValSeparator string) (*KVP, error) {
+func (parser *Parser) ParseLine(line string, keyValSeparator string) (*KVP, error) {
 	// get key and value as string types
-	kvpString, lineNotFoundErr := getStringKeyVal(line, keyValSeparator)
+	pKVPString, lineNotFoundErr := NewKeyVal(line, keyValSeparator)
 	if lineNotFoundErr != nil {
 		return nil, lineNotFoundErr
+	} else if pKVPString.Val == "" {
+		return nil, fmt.Errorf("Nil value for key in line: %q", line)
 	}
 
 	// find ConversionOperation for the key
-	key := (*kvpString).key
-	convOp, convOpNotFoundErr := findConversionOperationFromKey(key, parser.convOpMap)
-	if convOpNotFoundErr != nil {
-		return nil, convOpNotFoundErr
+	key := (*pKVPString).Key
+	convOp, ok := parser.ConvOpMap[key]
+	if !ok {
+		return nil, fmt.Errorf("ConversionOperation for key %q, not in map", key)
 	}
 
 	// apply conversion operation to the string value
-	valueString := (*kvpString).val
-	convertedValue, unableToConvertErr := convOp.apply(valueString)
+	valueString := (*pKVPString).Val
+	convertedValue, unableToConvertErr := convOp.Apply(valueString)
 	if unableToConvertErr != nil {
 		return nil, unableToConvertErr
 	}
 
 	// create struct for data and return
-	keyValuePair := KVP{key: key, value: convertedValue}
+	keyValuePair := KVP{Key: key, Value: convertedValue}
 	return &keyValuePair, nil
 }
