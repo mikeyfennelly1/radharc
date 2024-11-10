15f4a110bd32ab20164684943defeedb4f435c3c
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-11-10 18:28:46 +0000

    reformatted utils

--- core/parse/kvp2/utils.go
+++ core/parse/kvp2/utils.go
@@ -1,40 +1,30 @@
 package parse
 
 import (
-	"errors"
+	"fmt"
 	"strings"
 )
 
 type ConversionOperation struct {
-	apply func(string) (interface{}, error)
+	Apply func(string) (interface{}, error)
 }
 
-type StringKeyVal struct {
-	key string
-	val string
+type KeyValPair struct {
+	Key string
+	Val string
 }
 
-func getStringKeyVal(line string, keyValSeparator string) (*StringKeyVal, error) {
+func NewKeyVal(line string, separator string) (*KeyValPair, error) {
 	// return an error if separator not in line
-	if !strings.Contains(line, keyValSeparator) {
-		return nil, errors.New("KeyValSeparator not found in line: " + line)
+	if !strings.Contains(line, separator) {
+		return nil, fmt.Errorf("KeyValSeparator %q not found in line: %q", separator, line)
 	}
 
-	// split line and return &StringKeyVal for that line
-	keyVal := strings.Split(line, keyValSeparator)
-	StrKVP := StringKeyVal{
-		key: strings.TrimSpace(keyVal[0]),
-		val: strings.TrimSpace(keyVal[1]),
+	// split line and return &KeyValPair for that line
+	keyVal := strings.Split(line, separator)
+	StrKVP := KeyValPair{
+		Key: strings.TrimSpace(keyVal[0]),
+		Val: strings.TrimSpace(keyVal[1]),
 	}
 	return &StrKVP, nil
-}
-
-func findConversionOperationFromKey(key string, convOpMap map[string]ConversionOperation) (*ConversionOperation, error) {
-	operation, ok := convOpMap[key]
-	// if operation not in map return error
-	if !ok {
-		return nil, errors.New("ConversionOperation not found in map of ConversionOperations")
-	}
-
-	return &operation, nil
 }
