2662024f16d2612d59081848ef8efe5e2b98316f
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-11-11 10:48:52 +0000

    GetAllRunningPids created

--- core/vfiles/proc/all_pids.go
+++ core/vfiles/proc/all_pids.go
@@ -1,5 +1,28 @@
 package vfiles
 
-func getAllRunningPIDs() {
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+)
+
+func GetAllRunningPids() []int64 {
+	runningPids := make([]int64, 0)
+
+	entries, err := ioutil.ReadDir("/proc/")
+	if err != nil {
+		fmt.Errorf("Err")
+	}
+
+	for _, entry := range entries {
+		re := regexp.MustCompile(`^[0-9]+$`)
+		if re.MatchString(entry.Name()) {
+			pid, _ := strconv.ParseInt(entry.Name(), 10, 64)
+			runningPids = append(runningPids, pid)
+		}
+	}
+
+	return runningPids
 
 }
